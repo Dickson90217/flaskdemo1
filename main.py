874a07f5e7d75d58840369404633fc9382ff6b27
jerry <jerry@gmail.com>
2022-01-05 21:13:46 +0800

    完成pm2.5排序功能

--- main.py
+++ main.py
@@ -1,14 +1,47 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, make_response
 from datetime import datetime
-
+from pm25 import get_pm25
 
 app = Flask(__name__)
 
 
 @app.route('/')
-@app.route('/index')
 def index():
-    return render_template('./index.html')
+    name = 'jerry'
+    date = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    context = {
+        'name': name,
+        'date': date,
+    }
+
+    return render_template('./index.html', context=context)
+
+
+@app.route('/pm25', methods=['GET', 'POST'])
+def pm25():
+    if request.method == 'GET':
+        print('GET')
+        columns, values = get_pm25()
+
+    if request.method == 'POST':
+        if request.form.get('reverse'):
+            columns, values = get_pm25(type=1)
+        elif request.form.get('ascending'):
+            columns, values = get_pm25(type=2)
+
+    return render_template('./pm25.html', columns=columns, values=values)
+
+
+@app.route('/stocks')
+def stock():
+    all_stocks = [
+        {'分類': '日經指數', '指數': '22,920.30'},
+        {'分類': '韓國綜合', '指數': '2,304.59'},
+        {'分類': '香港恆生', '指數': '25,083.71'},
+        {'分類': '上海綜合', '指數': '3,380.68'}
+    ]
+
+    return render_template('./stock.html', stocks=all_stocks)
 
 
 @app.route('/bmi/name=<name>&height=<height>&weight=<weight>')
@@ -27,7 +60,7 @@ def get_sum(a, b):
     return str(total)
 
 
-@app.route('/today/<path:name>')
+@app.route('/today/<string:name>')
 def get_date(name):
     date = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
     return f'{name} {date}'
