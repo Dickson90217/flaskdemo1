8408d0a634c99bb048878953ae17c7cdd98804d0
Dickson <hhhh820605@gmail.com>
2022-01-10 21:44:26 +0800

    d

--- static/js/main.js
+++ static/js/main.js
@@ -1,28 +1,44 @@
 
-// 基于准备好的dom，初始化echarts实例
-var myChart = echarts.init(document.getElementById('main'));
+const chart1 = echarts.init(document.querySelector('#main'));
 
-// 指定图表的配置项和数据
-var option = {
-    title: {
-        text: 'ECharts 入门示例'
-    },
-    tooltip: {},
-    legend: {
-        data: ['销量']
-    },
-    xAxis: {
-        data: ['圍巾', '羊毛衫', '雪纺衫', '裤子', '高跟鞋', '袜子']
-    },
-    yAxis: {},
-    series: [
+$(document).ready(() => {
+    drawPM25();
+});
+
+function drawPM25() {
+
+    $.ajax(
         {
-            name: '销量',
-            type: 'bar',
-            data: [5, 20, 36, 10, 10, 20]
-        }
-    ]
-};
+            url: "/pm25-data",
+            type: "POST",
+            dataType: "json",
+            success: (data) => {
+                console.log(data);
+                var option = {
+                    title: {
+                        text: '全台pm2.5'
+                    },
+                    tooltip: {},
+                    legend: {
+                        data: ['pm2.5數值']
+                    },
+                    xAxis: {
+                        data: data['site']
+                    },
+                    yAxis: {},
+                    series: [
+                        {
+                            name: 'PM2.5',
+                            type: 'bar',
+                            data: data['pm25']
+                        }
+                    ]
+                };
 
-// 使用刚指定的配置项和数据显示图表。
-myChart.setOption(option);
+
+                chart1.setOption(option);
+            },
+            error: () => alert('讀取失敗')
+        }
+    );
+}
