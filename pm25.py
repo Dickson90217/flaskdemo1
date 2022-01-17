01ecd7a5ca574645dc5565f90611da31cbea1c70
Dickson <hhhh820605@gmail.com>
2022-01-17 21:42:22 +0800

    pm25.py update

--- pm25.py
+++ pm25.py
@@ -1,7 +1,4 @@
-from sqlite3.dbapi2 import Cursor
 import pandas as pd
-import path
-import sqlite3
 six_cities = ["臺北市", "新北市", "桃園市", "臺中市", "臺南市", "高雄市"]
 url = 'https://data.epa.gov.tw/api/v1/aqx_p_02?limit=1000&api_key=9be7b239-557b-4c10-9775-78cadfc555e9&sort=ImportDate%20desc&format=csv'
 df = pd.read_csv(url).dropna()
